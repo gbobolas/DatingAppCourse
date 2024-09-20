95bd5705a2baadd9e6c9fd1c150dde981e68b35e
gbobolas <67965688+gbobolas@users.noreply.github.com>
2024-09-20 22:56:10 +0300

    End of Section 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,18 +7,18 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
   title = 'DatingApp';
   users: any;
-  
+
   ngOnInit(): void {
-    this.http.get("https://localhost:5001/api/users").subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has completed'),
+    });
   }
 }
