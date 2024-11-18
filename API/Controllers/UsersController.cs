e8b31c28997a7a0f8ec4e102f6ce388e36c5f723
gbobolas <67965688+gbobolas@users.noreply.github.com>
2024-11-18 23:54:57 +0200

    Chapter 5 WIP

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,13 +9,14 @@ namespace API.Controllers;
 
 public class UsersController(DataContext context) : BaseApiController
 {
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
         var users = await context.Users.ToListAsync();
         return users;
     }
-
+    [Authorize]
     [HttpGet("{id:int}")] // api/users/1
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
